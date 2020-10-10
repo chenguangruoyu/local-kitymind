0f1fda23c74abd8b4bdfad38b24247429846a183
guozhixin-hj <guozhixin-hj@huajiao.tv>
2020-10-10 19:40:45 +0800

    暂提交部分代码

--- diy.js
+++ diy.js
@@ -1,6 +1,8 @@
 (function () {
+    
     var oldData;
     var html = '';
+
     html += '<a href="" class="diy export" data-type="json">导出json</a>',
     // html += '<a href="" class="diy export" data-type="md">导出md</a>',
     html += '<a href="" class="diy export" data-type="km">导出km</a>',
@@ -9,7 +11,8 @@
     html += '<a href="" class="diy export" data-type="png">导出png</a>',
     html += '<button class="diy input">',
     html += '导入<input type="file" id="fileInput" accept=".km,.txt,.md,.json" >',
-    html += '</button>';
+    html += '</button>',
+    html += '<a href="" class="diy clickSave" data-type="json">保存</a>';
 
     $('.editor-title').append(html);
 
@@ -26,6 +29,12 @@
         border: 'none',
         'border-right': '1px solid #ccc',
     });
+    $('.clickSave').css(
+        {
+            'background-color': 'yellow',
+            color: 'red',
+        }
+    );
     $('.input').css({
         'overflow': 'hidden',
         'position': 'relative',
@@ -86,7 +95,7 @@
             }
             var a = document.createElement("a"); //建立标签，模拟点击下载
             a.download = $('#node_text1').text() + '.' + type;
-            a.href = URL.createObjectURL(blob);
+            a.href = URL.createObjectURL(blob); // 生成一个指向blob的二进制数据，并赋值给a标签的href属性
             a.click();
 
         });
@@ -128,6 +137,67 @@
             reader.readAsText(file);
         });
     }
+
+    
+    // 保存
+    $(document).on('click', '.clickSave', function (event) {
+        event.preventDefault();
+        var $this = $(this),
+        type = $this.data('type'),
+        exportType;
+        switch (type) {
+        case 'km':
+            exportType = 'json';
+            break;
+        case 'md':
+            exportType = 'markdown';
+            break;
+        case 'svg':
+            exportType = 'svg';
+            break;
+        case 'txt':
+            exportType = 'text';
+            break;
+        case 'png':
+            exportType = 'png';
+            break;
+        default:
+            exportType = type;
+            break;
+        }
+
+        editor.minder.exportData(exportType).then(function (content) {
+            switch (exportType) {
+            case 'json':
+                console.log($.parseJSON(content));
+                break;
+            default:
+                console.log(content);
+                break;
+            }
+            var blob = new Blob();
+            switch (exportType) {
+            case 'png':
+                blob = dataURLtoBlob(content); //将base64编码转换为blob对象
+                break;
+            default:
+                blob = new Blob([content]);
+                break;
+            }
+            var a = document.createElement("a"); 
+            // 文件名
+            a.download = $('#node_text1').text() + '.' + type;
+            console.log(a.download);
+
+            // 调用保存文件接口
+
+            // saveAs(blob, a.download); 需要导入FileSaver.js
+
+            // a.href = URL.createObjectURL(blob);
+            // a.click();
+
+        });
+    });
 
 })();
 
